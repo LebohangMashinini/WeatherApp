5655ddb5470729894f5f868f9e38a76f9fc3c0fc
LebohangMashinini <palesal613@gmail.com>
2024-09-03 02:00:26 +0200

    Added function to fetch and display weather data based on user input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityNameElement = document.querySelector("#city-name");
+
+  cityNameElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function search(city) {
+  let apiKey = "b903ef34a6bc399tdc9c0dd7o68d5647";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityNameElement = document.querySelector("#city-name");
-  cityNameElement.innerHTML = searchInput.value;
+
+  search(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+search("Johannesburg")
+
