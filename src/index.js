e2f1f41f71983b3ee1aedf7f0c430ebc22619d5e
LebohangMashinini <palesal613@gmail.com>
2024-09-05 02:54:05 +0200

    Added weather API integration and dynamic weather icons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let iconElement = document.querySelector("#icon")
   let cityNameElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let dateTimeElement = document.querySelector("#date-time");
+  let date = new Date();
 
   cityNameElement.innerHTML = response.data.city;
+
+  dateTimeElement.innerHTML = formattedDate(date);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formattedDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = daysOfWeek[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function search(city) {
@@ -24,5 +58,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-search("Johannesburg")
-
+search("Johannesburg");
