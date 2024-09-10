632b2552b47340b0753e4f51dad6b52e004e16dd
LebohangMashinini <palesal613@gmail.com>
2024-09-10 22:50:19 +0200

    Added weather forecast API setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let iconElement = document.querySelector("#icon")
+  let iconElement = document.querySelector("#icon");
   let cityNameElement = document.querySelector("#city-name");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
@@ -10,13 +10,14 @@ function updateWeather(response) {
   let date = new Date();
 
   cityNameElement.innerHTML = response.data.city;
-
   dateTimeElement.innerHTML = formattedDate(date);
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   descriptionElement.innerHTML = response.data.condition.description;
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 
 function formattedDate(date) {
@@ -55,7 +56,42 @@ function handleSearchSubmit(event) {
   search(searchInput.value);
 }
 
+function getForecast(city) {
+let apiKey = "b903ef34a6bc399tdc9c0dd7o68d5647";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+
+axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="full-weather-forecast">
+        <div class="weather-forecast-day">${day}</div>
+        <div class="weather-forecast-icon">☀️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>24°</strong>
+          </div>
+          <div class="weather-forecast-temperature">20°</div>
+        </div>
+      </div>
+      `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 search("Johannesburg");
+
